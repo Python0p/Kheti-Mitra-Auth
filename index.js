9fd317e055341779f6be3a988caea77c22e24882
Abhishek Gupta <abhishekgup232326@gmail.com>
2024-12-25 09:56:42 +0530

    added frontend

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // Add cookie-parser middleware
 
 // Serve static files (HTML, CSS, JS) from 'views' folder
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(__dirname));
 
 // Connect to MongoDB first
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
